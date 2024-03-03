b880ff10e737920226dc63349b00fa7e6d3cbbae
defaulterrr <thedefaulterrr@gmail.com>
2024-03-03 20:21:18 +0300

    feat: simplify demo

--- go.mod
+++ go.mod
@@ -3,13 +3,16 @@ module github.com/defaulterrr/protoc-gen-pgx
 go 1.21.6
 
 require (
-	github.com/jackc/pgx/v5 v5.5.2
+	github.com/jackc/pgx/v5 v5.5.3
+	github.com/spf13/pflag v1.0.5
+	golang.org/x/text v0.14.0
 	google.golang.org/protobuf v1.32.0
 )
 
 require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	golang.org/x/crypto v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 )
--- internal/plugin/plugin.go
+++ internal/plugin/plugin.go
@@ -1,37 +1,119 @@
 package plugin
 
 import (
+	"bytes"
+	"fmt"
+	"strings"
+	"text/template"
+
 	pb "github.com/defaulterrr/protoc-gen-pgx/pb/annotations"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/types/descriptorpb"
 )
 
-func FindAnnotatedMessages(messages []*protogen.Message) []*protogen.Message {
-	annotatedMessages := []*protogen.Message{}
-
-	for _, message := range messages {
-		if shouldGenerate(message.Desc.Options().(*descriptorpb.MessageOptions)) {
-			annotatedMessages = append(annotatedMessages, message)
-		}
-	}
-
-	return annotatedMessages
+type Meta struct {
+	TableName           string
+	MainTypeCapitalized string
+	MainTypeLowercased  string
+	Types               []Fields
 }
 
-func shouldGenerate(in *descriptorpb.MessageOptions) bool {
+type Fields struct {
+	Name               string
+	GoType             string
+	MainTypeLowercased string
+}
+
+func GenerateFileForType(
+	typeToGenerate Meta,
+	template *template.Template,
+) ([]byte, error) {
+	var buf bytes.Buffer
+
+	if err := template.Execute(&buf, typeToGenerate); err != nil {
+		return nil, fmt.Errorf("failed to generate type using given template: %w", err)
+	}
+
+	return buf.Bytes(), nil
+}
+
+func MetaFromProtobufType(message *protogen.Message) (Meta, error) {
+	tableName := PostgresTableNameForMessage(message)
+
+	capitalized := cases.Title(language.English).String(tableName)
+	lowercased := cases.Lower(language.English).String(tableName)
+
+	opts := Meta{
+		TableName:           tableName,
+		MainTypeCapitalized: capitalized,
+		MainTypeLowercased:  lowercased,
+		Types:               []Fields{},
+	}
+
+	for _, field := range message.Fields {
+		name := string(field.Desc.Name())
+		gotype, err := PostgresTypeFromProtobufType(field)
+		if err != nil {
+			return opts, fmt.Errorf("failed to map message %s to postgres type: %w", message.Desc.Name(), err)
+		}
+		opts.Types = append(opts.Types, Fields{
+			Name:               name,
+			GoType:             gotype,
+			MainTypeLowercased: lowercased,
+		})
+	}
+
+	return opts, nil
+}
+
+func PostgresTypeFromProtobufType(pbtype *protogen.Field) (string, error) {
+	switch pbtype.Desc.Kind() {
+	case protoreflect.BoolKind:
+		return "bool", nil
+	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind:
+		return "int32", nil
+	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
+		return "uint32", nil
+	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
+		return "int64", nil
+	case protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+		return "uint64", nil
+	case protoreflect.FloatKind:
+		return "float32", nil
+	case protoreflect.DoubleKind:
+		return "float64", nil
+	case protoreflect.StringKind:
+		return "string", nil
+	case protoreflect.BytesKind:
+		return "[]byte", nil
+	default:
+		return "", fmt.Errorf("unsupported type: %s", pbtype.Desc.Kind().String())
+	}
+}
+
+func PostgresTableNameForMessage(message *protogen.Message) string {
+	tableName, ok := getTableNameFromOptions(message.Desc.Options().(*descriptorpb.MessageOptions))
+	if ok {
+		return tableName
+	}
+	return strings.ToLower(string(message.Desc.Name()))
+}
+
+func getTableNameFromOptions(in *descriptorpb.MessageOptions) (string, bool) {
 	if in == nil {
-		return false
+		return "", false
 	}
 
-	v := proto.GetExtension(in, pb.E_ShouldGenerate)
+	v := proto.GetExtension(in, pb.E_TableName)
 	if v == nil {
-		return false
+		return "", false
 	}
 
-	shouldGenerate, ok := v.(bool)
-	if !ok {
-		return false
-	}
-	return shouldGenerate
+	shouldGenerate, ok := v.(string)
+	return shouldGenerate, ok
 }
